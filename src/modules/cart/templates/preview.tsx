f8e81ce2df860dfa8dff0e9ba78a6d35897e643f
TinaS2024 <Tina.Schmidtbauer@bolasys.de>
2026-05-08 09:37:11 +0200

    ...

--- src/modules/cart/templates/preview.tsx
+++ src/modules/cart/templates/preview.tsx
@@ -9,9 +9,11 @@ import SkeletonLineItem from "@modules/skeletons/components/skeleton-line-item"
 
 type ItemsTemplateProps = {
   cart: HttpTypes.StoreCart
+  productTitles?: Record<string, string>
+  variantTitles?: Record<string, string>
 }
 
-const ItemsPreviewTemplate = ({ cart }: ItemsTemplateProps) => {
+const ItemsPreviewTemplate = ({ cart, productTitles, variantTitles }: ItemsTemplateProps) => {
   const items = cart.items
   const hasOverflow = items && items.length > 4
 
@@ -30,10 +32,19 @@ const ItemsPreviewTemplate = ({ cart }: ItemsTemplateProps) => {
                   return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
                 })
                 .map((item) => {
+                  const productId = item.variant?.product?.id
+                  const title = productId ? productTitles?.[productId] : undefined
+
+                  const variantId = item.variant?.id
+                  const variantTitle = variantId
+                    ? variantTitles?.[variantId]
+                    : undefined
                   return (
                     <Item
                       key={item.id}
                       item={item}
+                      title={title}
+                      variantTitle={variantTitle}
                       type="preview"
                       currencyCode={cart.currency_code}
                     />
